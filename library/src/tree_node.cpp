806123acf8d261c2cfe044e63fdc6e0ea60f72f5
Alan Fischer <af.ayala.32@gmail.com>
2025-06-23 16:29:15 -0500

    a2

--- library/src/tree_node.cpp
+++ library/src/tree_node.cpp
@@ -940,11 +940,13 @@ void CommAllToAll::ExecuteAsync(const rocfft_plan     plan,
             log_plan("Using MPI_Ialltoall\n");
 
 
-            if(LOG_TRACE_ENABLED())
-        {
-            auto& os = *LogSingleton::GetInstance().GetTraceOS();
-            Print(os, 1);
-        }                
+MPI_Comm comm = plan->desc.use_subcomm ? plan->desc.subcomm : plan->desc.mpi_comm;
+
+if(LOG_TRACE_ENABLED())
+{
+    auto& os = *LogSingleton::GetInstance().GetTraceOS();
+    Print(os, 1, comm);  // <- Pass it in
+}
 
         const int send_count_bytes = static_cast<int>(sendCounts[0] * elem_size);
 
@@ -1041,7 +1043,7 @@ void CommAllToAll::Wait()
     }
 }
 
-void CommAllToAll::Print(rocfft_ostream& os, const int indent) const
+void CommAllToAll::Print(rocfft_ostream& os, const int indent, MPI_Comm comm) const
 {
     std::string indentStr;
     int         i = indent;
