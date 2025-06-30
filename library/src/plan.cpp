0e6b419919e4590b34897ed88c394af61850674f
Alan Fischer <af.ayala.32@gmail.com>
2025-06-30 10:14:23 -0400

    a3

--- library/src/plan.cpp
+++ library/src/plan.cpp
@@ -2992,6 +2992,8 @@ rocfft_status rocfft_plan_create_internal(rocfft_plan                   plan,
                 // multi-device was not requested, or we can't optimize for
                 // that case), fall back to single-device plan
 
+                std::cout << "falling back to single-device plan" << std::endl;
+
                 NodeMetaData rootPlanData(nullptr);
                 set_rootplan_params(plan, rootPlanData);
                 rootPlanData.deviceProp = get_curr_device_prop();
