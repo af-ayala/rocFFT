f0dae0fb15d12ce4e96527317366b12bf68fd1cd
Alan Fischer <af.ayala.32@gmail.com>
2025-06-30 12:45:04 -0400

    new intermediate field

--- library/src/plan.cpp
+++ library/src/plan.cpp
@@ -2431,12 +2431,9 @@ bool rocfft_plan_t::BuildOptMultiDevicePlan()
         std::vector<size_t> midItems2;
         std::vector<size_t> outputItems;
 
-auto MakeTempBufferCtor = [](const std::vector<rocfft_brick_t>& bricks)
+return [](size_t userIdx, int comm_rank)
 {
-    return [&bricks](size_t userIdx, int comm_rank)
-    {
-        return BufferPtr::temp(std::make_shared<InternalTempBuffer>(comm_rank));
-    };
+    return BufferPtr::temp(std::make_shared<InternalTempBuffer>(comm_rank));
 };
 
         // first set of local FFTs along X
