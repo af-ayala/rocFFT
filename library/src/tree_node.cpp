8ba75130868b3333cee61709913b0bc7f45e864b
Alan Fischer <af.ayala.32@gmail.com>
2025-07-23 17:52:41 -0500

    2

--- library/src/tree_node.cpp
+++ library/src/tree_node.cpp
@@ -1016,7 +1016,7 @@ void CommAllToAll::ExecuteAsync(const rocfft_plan     plan,
 #ifdef ROCFFT_MPI_ENABLE
 
 std::cout <<  "uniform_counts inside ExecuteAsync" << std::endl;
-std::cout << "uniform_counts: " << uniform_counts << ", use_subcomm: " << use_subcomm << std::endl;
+std::cout << "uniform_counts: " << uniform_counts << std::endl;
 std::cout << "sendCounts: ";
 for(auto x : sendCounts) std::cout << x << " ";
 std::cout << "\n recvCounts: ";
