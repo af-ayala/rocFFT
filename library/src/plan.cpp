a7ca39e6e4e5585a1042be9f9756cabedd990123
Alan Fischer <af.ayala.32@gmail.com>
2025-07-25 05:47:24 -0500

    5

--- library/src/plan.cpp
+++ library/src/plan.cpp
@@ -2733,6 +2733,10 @@ if(num_split_dims_in >= 2 && num_split_dims_out >= 2 && !use_intermediate_slabs)
                 transposeNumber++,
                 (in_pencil_comm ? std::move(pencil_comm) : MPI_Comm_wrapper_t{})
             );
+
+            std::cout << " size of currentBufs " << currentBufs.size << std::endl;
+            std::cout << " size of tempBufs " << tempBufs.size << std::endl;
+
             currentField = nextField;
             currentBufs = tempBufs;
             currentAntecedents = transposeItems;
