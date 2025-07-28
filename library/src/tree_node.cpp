b457159c15e203958e611af9643728d57c90eb19
Alan Fischer <af.ayala.32@gmail.com>
2025-07-28 03:22:46 -0500

    3

--- library/src/tree_node.cpp
+++ library/src/tree_node.cpp
@@ -914,7 +914,12 @@ void CommAllToAll::ExecuteAsync(const rocfft_plan     plan,
 
     // check that we have as many elems in our count/offset buffers as
     // we have ranks
-    const size_t num_ranks = plan->get_local_comm_size();
+    const size_t num_ranks;
+
+    if(subcomm)
+        MPI_Comm_size(subcomm, &num_ranks);
+    else
+        subcomm_size = plan->get_local_comm_size();   
 
     std::cout << "[DEBUG][Rank " << local_comm_rank << "] num_ranks=" << num_ranks
             << " sendOffsets.size()=" << sendOffsets.size()
